b4d87fc0a090215620d5d3722d1db1ec83e02b7e
pankajackson <pankajackson@live.co.uk>
2022-05-20 15:15:59 +0530

    Removed log stdout

--- reportgenerator.py
+++ reportgenerator.py
@@ -29,10 +29,8 @@ def get_es_connection():
 def calc_size_unit(size=0, unit='B'):
     if size == 0:
         return {'size': 0, 'unit': 'B'}
-    print('size in B: {siz}'.format(siz=size))
     size = ((size/1024)/1024)/1024
     unit = 'GB'
-    print('size in G: {siz}'.format(siz=size))
     return '{size}{unit}'.format(size=round(size, 2),unit=unit)
 
 
