04abe4bca778bfcdc2f9130215293386c616bb62
Pankaj Jackson <pankajackson@live.co.uk>
2022-05-24 21:13:43 +0530

    Added Mac OS Intallation support

--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 pip3 uninstall -y -r requirements.txt || pip uninstall -y -r requirements.txt
-sudo rm -rf /bin/esreportgen
+sudo rm -rf /bin/esreportgen || sudo rm -rf /usr/local/bin/esreportgen
